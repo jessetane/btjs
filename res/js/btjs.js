2d9a725f70fdb9f4fb3973776e65d4df223e6965
Smashplug <brian@smashplug.com>
2012-06-25 11:39:10 -0400

    Recommit

---
+++ res/js/btjs.js
@@ -0,0 +1,63 @@
+var _intervalUpdateState;
+
+var GameState = {
+	grid : undefined,
+	locs : undefined,
+	owners : undefined,
+	start_time : undefined,
+	units : undefined,
+	hps : undefined,
+
+	init : function(result) {
+		this.grid = result.initial_state.grid.grid;
+		this.locs = result.initial_state.init_locs;
+		this.owners = result.initial_state.owners;
+		this.start_time = result.initial_state.start_time;
+		this.units = result.initial_state.units;
+	},
+	update : function(result) {
+		this.clearGridContents();
+		return this.updateUnitLocations(result.locs);
+
+	},
+	clearGridContents : function() {
+		for (var x in this.grid.tiles) {
+			for (var y in this.grid.tiles[x]) {
+				var tile = this.grid.tiles[x][y];
+				tile.contents = null;
+			}
+		}
+	},
+	updateUnitLocations : function(locs) {
+		var change = false;
+		for (var l in locs) {
+			if(this.units[l].location > locs[l]){
+				this.units[l].location = locs[l];
+				this.grid.tiles[locs[l][0]][locs[l][1]].tile.contents = this.units[l];
+				change = true;
+			}
+		}
+
+		this.locs = locs;
+		return change;
+	},
+	getUnitIdByName : function(name) {
+		for (var id in this.units) {
+			var unit = this.units[id];
+			if (unit.scient && unit.scient.name == name)
+				return id;
+			if (unit.nescient && unit.nescient.name == name)
+				return id;
+		}
+
+		return false;
+	},
+	getUnitIdByContents : function(contents) {
+		if (contents.scient)
+			return this.getUnitIdByName(contents.scient.name);
+		if (contents.nescient)
+			return this.getUnitIdByName(contents.nescient.name);
+
+		return false;
+	}
+}
